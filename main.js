82fa8f2899711292160aac9d461b6525b080441a
konpou <kon.poulo@gmail.com>
2021-05-06 17:44:06 +0300

    Added question image after clicking play again

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-'use strict';
-const rock_div = document.getElementById('rock');
-const paper_div = document.getElementById('paper');
-const scissors_div = document.getElementById('scissors');
-let round_span = document.querySelector('.roundCount');
+"use strict";
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+let round_span = document.querySelector(".roundCount");
 let userScore = 0;
 let compScore = 0;
 let score = 0;
-let points_div = document.querySelector('.points-0');
-let points_div2 = document.querySelector('.points-1');
-const questionmarkImage = document.querySelector('.quest');
-let button = document.querySelector('.btn');
+let points_div = document.querySelector(".points-0");
+let points_div2 = document.querySelector(".points-1");
+const questionmarkImage = document.querySelector(".quest");
+let button = document.querySelector(".btn");
 let playing = true;
 
 // Initialization function
@@ -19,8 +19,9 @@ const init = function () {
   compScore = 0;
   score = 0;
   round_span.textContent = score;
-  for (let i = 0; i < 3; i++) points_div.children[i].style.color = '';
-  for (let i = 0; i < 3; i++) points_div2.children[i].style.color = '';
+  for (let i = 0; i < 3; i++) points_div.children[i].style.color = "";
+  for (let i = 0; i < 3; i++) points_div2.children[i].style.color = "";
+  questionmarkImage.src = `questionmark-.png`;
 };
 
 // Computer's pick functionality
@@ -36,8 +37,8 @@ function compPick() {
 // Game functionality
 function game(userPick) {
   const computerPick = compPick();
-  console.log('User ' + userPick);
-  console.log('computer ' + computerPick);
+  console.log("User " + userPick);
+  console.log("computer " + computerPick);
 
   if (playing) {
     let isWin;
@@ -48,14 +49,14 @@ function game(userPick) {
     else isWin = false;
 
     if (tie === true) {
-      console.log('tie');
+      console.log("tie");
     } else if (isWin === true) {
-      console.log('won');
-      points_div.children[userScore].style.color = 'red';
+      console.log("won");
+      points_div.children[userScore].style.color = "red";
       userScore++;
     } else if (!isWin) {
-      points_div2.children[compScore].style.color = 'red';
-      console.log('lost');
+      points_div2.children[compScore].style.color = "red";
+      console.log("lost");
       compScore++;
     }
 
@@ -71,15 +72,15 @@ function game(userPick) {
 // Function for user's pick
 function userPick() {
   // rock
-  rock_div.addEventListener('click', function () {
+  rock_div.addEventListener("click", function () {
     game(0);
   });
   // paper
-  paper_div.addEventListener('click', function () {
+  paper_div.addEventListener("click", function () {
     game(1);
   });
   // scissors
-  scissors_div.addEventListener('click', function () {
+  scissors_div.addEventListener("click", function () {
     game(2);
   });
 }
@@ -87,7 +88,7 @@ function userPick() {
 userPick();
 
 // Play again button
-button.addEventListener('click', function () {
+button.addEventListener("click", function () {
   init();
 });
 
