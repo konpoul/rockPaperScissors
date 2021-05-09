ca2ab09383f6748403712b3d07607b2f7aab3b2d
konpou <kon.poulo@gmail.com>
2021-05-09 13:35:16 +0300

    Added alert delay and refactored code

--- main.js
+++ main.js
@@ -9,19 +9,21 @@ let score = 0;
 let points_div = document.querySelector(".points-0");
 let points_div2 = document.querySelector(".points-1");
 const questionmarkImage = document.querySelector(".quest");
+const resultMessage = document.querySelector(".result-message");
 let button = document.querySelector(".btn");
 let playing = true;
+var timeoutID;
+var timeoutID2;
 
 // Initialization function
 const init = function () {
   playing = true;
-  userScore = 0;
-  compScore = 0;
   score = 0;
   round_span.textContent = score;
   for (let i = 0; i < 3; i++) points_div.children[i].style.color = "";
   for (let i = 0; i < 3; i++) points_div2.children[i].style.color = "";
   questionmarkImage.src = `questionmark.png`;
+  resultMessage.classList.add("hidden");
 };
 
 // Computer's pick functionality
@@ -37,36 +39,42 @@ function compPick() {
 // Game functionality
 function game(userPick) {
   const computerPick = compPick();
-  console.log("User " + userPick);
-  console.log("computer " + computerPick);
 
   if (playing) {
     let isWin;
     let tie;
+    resultMessage.classList.remove("hidden");
+    // tie, win, lose logic
     if (userPick === computerPick) tie = true;
     else if ((userPick - computerPick) % 3 == 1) isWin = true;
     else if ((userPick - computerPick) % 3 == -2) isWin = true;
     else isWin = false;
 
+    // Hearts color turns red in win of each player also win-tie-lose message
     if (tie === true) {
-      console.log("tie");
+      resultMessage.textContent = "TIE";
     } else if (isWin === true) {
-      console.log("won");
       points_div.children[userScore].style.color = "red";
       userScore++;
+      resultMessage.textContent = "WIN";
     } else if (!isWin) {
       points_div2.children[compScore].style.color = "red";
-      console.log("lost");
       compScore++;
+      resultMessage.textContent = "LOST";
     }
 
     // Counting rounds
-    let roundCound = parseInt(round_span.textContent);
-    roundCound++;
-    round_span.textContent = roundCound;
+    let roundCount = parseInt(round_span.textContent);
+    roundCount++;
+    round_span.textContent = roundCount;
   }
-  // Messages when game ends
-  endGameMessage();
+  // When score is 3 game ends
+  if (userScore === 3 || compScore === 3) {
+    endGame();
+    playing = false;
+    compScore = 0;
+    userScore = 0;
+  }
 }
 
 // Function for user's pick
@@ -87,21 +95,23 @@ function userPick() {
 
 userPick();
 
+// End game with alert delay functionality
+const endGame = () => {
+  if (userScore === 3)
+    timeoutID = window.setTimeout(
+      window.alert,
+      250,
+      `You won with score ${userScore}:${compScore} press play again button`
+    );
+  else if (compScore === 3)
+    timeoutID2 = window.setTimeout(
+      window.alert,
+      250,
+      `You lost with score ${userScore}:${compScore} press play again button`
+    );
+};
+
 // Play again button
 button.addEventListener("click", function () {
   init();
 });
-
-// End game message function
-const endGameMessage = () => {
-  if (userScore === 3)
-    alert(
-      `You won with score ${userScore}:${compScore} press play again button`
-    );
-  else if (compScore === 3)
-    alert(
-      `You lost with score ${userScore}:${compScore} press play again button`
-    );
-
-  if (userScore === 3 || compScore == 3) playing = false;
-};
